464207820041be2d557270a9e93c5c9dc6880d19
jeremydmiller <jeremydmiller@yahoo.com>
2013-05-30 08:43:29 -0500

    got some UT coverage on SamlAuthenticationStrategy

---
+++ src/FubuMVC.Saml2.Testing/SamlAuthenticationRegistryTester.cs
@@ -0,0 +1,131 @@
+﻿using FubuCore.Binding;
+using FubuMVC.Authentication;
+using FubuSaml2;
+using FubuSaml2.Encryption;
+using FubuSaml2.Validation;
+using FubuTestingSupport;
+using NUnit.Framework;
+using Rhino.Mocks;
+using System.Collections.Generic;
+
+namespace FubuMVC.Saml2.Testing
+{
+    [TestFixture]
+    public class when_trying_to_apply_with_no_saml : InteractionContext<SamlAuthenticationStrategy>
+    {
+        private InMemoryRequestData theRequestData;
+        private AuthResult theResult;
+
+        protected override void beforeEach()
+        {
+            theRequestData = new InMemoryRequestData();
+            Services.Inject<IRequestData>(theRequestData);
+
+            Services.PartialMockTheClassUnderTest();
+            ClassUnderTest.Expect(x => x.ProcessSamlResponseXml("anything"))
+                          .IgnoreArguments().Repeat.Never();
+
+
+
+            theResult = ClassUnderTest.TryToApply();
+        }
+
+        [Test]
+        public void does_not_try_to_process_any_saml()
+        {
+            ClassUnderTest.VerifyAllExpectations();
+        }
+        
+        [Test]
+        public void the_result_is_just_false()
+        {
+            theResult.Continuation.ShouldBeNull();
+            theResult.Success.ShouldBeFalse();
+        }
+    }
+
+    [TestFixture]
+    public class when_try_to_apply_with_saml_response : InteractionContext<SamlAuthenticationStrategy>
+    {
+        private AuthResult theDirectoryResult;
+        private InMemoryRequestData theRequestData;
+        private string theResponseXml;
+        private AuthResult theResult;
+
+        protected override void beforeEach()
+        {
+            theDirectoryResult = new AuthResult();
+            MockFor<ISamlDirector>().Stub(x => x.Result()).Return(theDirectoryResult);
+
+
+            theResponseXml = "<Response />";
+
+            theRequestData = new InMemoryRequestData();
+            theRequestData["SamlResponse"] = theResponseXml;
+            theRequestData.Value("SamlResponse").ShouldNotBeNull();
+
+            Services.Inject<IRequestData>(theRequestData);
+
+            Services.PartialMockTheClassUnderTest();
+            ClassUnderTest.Expect(x => x.ProcessSamlResponseXml(theResponseXml));
+
+            theResult = ClassUnderTest.TryToApply();
+        }
+
+        [Test]
+        public void should_have_processed_the_saml_xml()
+        {
+            ClassUnderTest.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void should_return_the_result_from_the_director()
+        {
+            theResult.ShouldBeTheSameAs(theDirectoryResult);
+        }
+    }
+
+    [TestFixture]
+    public class when_processing_the_saml_xml : InteractionContext<SamlAuthenticationStrategy>
+    {
+        private ISamlValidationRule[] theRules;
+        private SamlResponse theResponse;
+        private string theXml;
+        private ISamlResponseStrategy[] theHandlers;
+
+        protected override void beforeEach()
+        {
+            theRules = Services.CreateMockArrayFor<ISamlValidationRule>(5);
+
+            theResponse = new SamlResponse();
+            theXml = "<Response />";
+
+            MockFor<ISamlResponseReader>().Stub(x => x.Read(theXml)).Return(theResponse);
+
+            theHandlers = Services.CreateMockArrayFor<ISamlResponseStrategy>(3);
+            theHandlers[2].Stub(x => x.CanHandle(theResponse)).Return(true);
+
+            ClassUnderTest.ProcessSamlResponseXml(theXml);
+        }
+
+        [Test]
+        public void should_run_all_the_validation_rules()
+        {
+            theRules.Each(rule => {
+                rule.AssertWasCalled(x => x.Validate(theResponse));
+            });
+        }
+
+        [Test]
+        public void uses_the_first_saml_response_handler_that_matches()
+        {
+            var theDirector = MockFor<ISamlDirector>();
+
+            // The [2] handler matches via its CanHandle method, the others don't
+            theHandlers[2].AssertWasCalled(x => x.Handle(theDirector, theResponse));
+
+            theHandlers[0].AssertWasNotCalled(x => x.Handle(theDirector, theResponse));
+            theHandlers[1].AssertWasNotCalled(x => x.Handle(theDirector, theResponse));
+        }
+    }
+}
---
+++ src/FubuMVC.Saml2/SamlAuthenticationStrategy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FubuCore.Binding;
+using FubuMVC.Authentication;
+using FubuSaml2.Encryption;
+using FubuSaml2.Validation;
+
+namespace FubuMVC.Saml2
+{
+    public class SamlAuthenticationStrategy : IAuthenticationStrategy
+    {
+        public static readonly AuthResult DoesNotApply = new AuthResult{Success = false};
+
+        private readonly IRequestData _requestData;
+        private readonly ISamlDirector _director;
+        private readonly ISamlResponseReader _reader;
+        private readonly IEnumerable<ISamlValidationRule> _rules;
+        private readonly IEnumerable<ISamlResponseStrategy> _strategies;
+
+        public SamlAuthenticationStrategy(IRequestData requestData, ISamlDirector director, ISamlResponseReader reader, IEnumerable<ISamlValidationRule> rules, IEnumerable<ISamlResponseStrategy> strategies)
+        {
+            _requestData = requestData;
+            _director = director;
+            _reader = reader;
+            _rules = rules;
+            _strategies = strategies;
+        }
+
+        public AuthResult TryToApply()
+        {
+            AuthResult result = AuthResult.Failed();
+
+            // TODO -- make sure we've got error handling here
+            _requestData.Value("SamlResponse", v => {
+                // TODO -- log here
+                var xml = v.RawValue as string;
+                ProcessSamlResponseXml(xml);
+
+                result = _director.Result();
+            });
+
+            return result;
+        }
+
+        public virtual void ProcessSamlResponseXml(string xml)
+        {
+            var response = _reader.Read(xml);
+
+            _rules.Each(x => x.Validate(response));
+
+            // TODO -- Make sure there's a default one in here?
+            var handler = _strategies.First(x => x.CanHandle(response));
+            // TODO -- do something if we cannot select a handler.  Default message maybe
+            handler.Handle(_director, response);
+        }
+
+        public bool Authenticate(LoginRequest request)
+        {
+            return false;
+        }
+    }
+}
